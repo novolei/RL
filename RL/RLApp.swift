ea0f3eec33579481aa057126ff974899cd78da52
Ryan Liu <aresray@126.com>
2022-09-05 15:23:09 +0800

    Initial Commit

---
+++ RL/RLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLApp.swift
+//  RL
+//
+//  Created by Ryan Liu on 2022/9/5.
+//
+
+import SwiftUI
+
+@main
+struct RLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
